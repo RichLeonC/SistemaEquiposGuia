60ed91793c1562d674b6c4fd7d423bcdf293f1a1
RichLeonC <r29leonc@gmail.com>
2023-04-25 20:50:37 -0600

    Backend Nodejs activo
    Usando express.js como framework

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+//Settings
+app.set('port',process.env.PORT || 4000);
+
+app.use(express.json());
+app.use(morgan("common"));
+app.use(cors());
+
+app.listen(app.get('port'),()=>{ //Va abrir el server en el puerto 4000
+    console.log('Server on port',app.get('port'));
+})
+
+
+
