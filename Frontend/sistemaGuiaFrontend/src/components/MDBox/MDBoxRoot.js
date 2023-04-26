6348549181866dd89a16c3483fca659f79708ce0
RichLeonC <r29leonc@gmail.com>
2023-04-25 19:40:45 -0600

    Update template

--- Frontend/sistemaGuiaFrontend/src/components/MDBox/MDBoxRoot.js
+++ Frontend/sistemaGuiaFrontend/src/components/MDBox/MDBoxRoot.js
@@ -1,17 +1,4 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // @mui material components
 import Box from "@mui/material/Box";
@@ -71,6 +58,7 @@ export default styled(Box)(({ theme, ownerState }) => {
     "grey-700",
     "grey-800",
     "grey-900",
+    "#140e39"
   ];
 
   const validBorderRadius = ["xs", "sm", "md", "lg", "xl", "xxl", "section"];
--- Frontend/sistemaGuiaFrontend/src/layouts/authentication/sign-in/index.js
+++ Frontend/sistemaGuiaFrontend/src/layouts/authentication/sign-in/index.js
@@ -22,12 +22,7 @@ import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import Switch from "@mui/material/Switch";
 import Grid from "@mui/material/Grid";
-import MuiLink from "@mui/material/Link";
 
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import GoogleIcon from "@mui/icons-material/Google";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -50,7 +45,6 @@ function Basic() {
     <BasicLayout image={bgImage}>
       <Card>
         <MDBox
-          variant="gradient"
           bgColor="info"
           borderRadius="lg"
           coloredShadow="info"
@@ -60,55 +54,27 @@ function Basic() {
           mb={1}
           textAlign="center"
         >
-          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-            Sign in
-          </MDTypography>
+          <br/>
+          <img src="https://tecdigital.tec.ac.cr/images/logoTECBLANCO.png"width={"250rem"}></img>
           <Grid container spacing={3} justifyContent="center" sx={{ mt: 1, mb: 2 }}>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <FacebookIcon color="inherit" />
-              </MDTypography>
-            </Grid>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <GitHubIcon color="inherit" />
-              </MDTypography>
-            </Grid>
-            <Grid item xs={2}>
-              <MDTypography component={MuiLink} href="#" variant="body1" color="white">
-                <GoogleIcon color="inherit" />
-              </MDTypography>
-            </Grid>
           </Grid>
         </MDBox>
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
             <MDBox mb={2}>
-              <MDInput type="email" label="Email" fullWidth />
+              <MDInput type="email" label="Correo" fullWidth />
             </MDBox>
             <MDBox mb={2}>
-              <MDInput type="password" label="Password" fullWidth />
-            </MDBox>
-            <MDBox display="flex" alignItems="center" ml={-1}>
-              <Switch checked={rememberMe} onChange={handleSetRememberMe} />
-              <MDTypography
-                variant="button"
-                fontWeight="regular"
-                color="text"
-                onClick={handleSetRememberMe}
-                sx={{ cursor: "pointer", userSelect: "none", ml: -1 }}
-              >
-                &nbsp;&nbsp;Remember me
-              </MDTypography>
+              <MDInput type="password" label="Contraseña" fullWidth />
             </MDBox>
             <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth>
-                sign in
+              <MDButton color="info" fullWidth>
+                Aceptar
               </MDButton>
             </MDBox>
             <MDBox mt={3} mb={1} textAlign="center">
               <MDTypography variant="button" color="text">
-                Don&apos;t have an account?{" "}
+                Olvidaste la contraseña?{" "}
                 <MDTypography
                   component={Link}
                   to="/authentication/sign-up"
@@ -117,7 +83,7 @@ function Basic() {
                   fontWeight="medium"
                   textGradient
                 >
-                  Sign up
+                  Recuperar
                 </MDTypography>
               </MDTypography>
             </MDBox>
