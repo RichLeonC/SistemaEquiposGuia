1356ba78aa6f01ed3a1cda21099a32c204fdc4a8
Andres <anmasis@ic-itcr.ac.cr>
2023-05-03 19:57:32 -0600

    CRUD y Crear profesor

--- BaseDatos/Procedures/crearProfesor.sql
+++ BaseDatos/Procedures/crearProfesor.sql
@@ -1,4 +1,5 @@
 <<<<<<< refs/remotes/origin/main
+<<<<<<< refs/remotes/origin/main
 CREATE OR ALTER PROCEDURE crear_profesor
     @cedulaProfesor INT,
     @codigo VARCHAR(10),
@@ -37,43 +38,50 @@ BEGIN
         PRINT 'Ocurrio un error: ' + ERROR_MESSAGE()
 =======
 CREATE PROCEDURE insertar_profesor
+=======
+CREATE OR ALTER PROCEDURE crear_profesor
+>>>>>>> CRUD y Crear profesor
     @cedulaProfesor INT,
     @codigo VARCHAR(10),
-    @esCordinador INT,
+    @esCordinador INT = 0,
 
-	@cedulaUsuario INT,
 	@nombre VARCHAR(25),
-	@segundonombre VARCHAR(25),
+	@segundonombre VARCHAR(25) = NULL,
 	@apellido1 VARCHAR(25),
-	@apellido2 VARCHAR(25),
+	@apellido2 VARCHAR(25) = NULL,
 	@correo VARCHAR(100),
 	@clave VARCHAR(100),
 	@celular INT,
-	@rol VARCHAR(20),
+	@rol VARCHAR(20) = 'Profesor',
 
-	@cedulaPersonal INT,
 	@idSede INT,
-	@telOficina INT,
-	@foto VARCHAR(255)
+	@telOficina INT = NULL,
+	@foto VARCHAR(255) = NULL
 AS
 BEGIN
     BEGIN TRY
+		
+		INSERT INTO [dbo].[usuario]([cedulaUsuario],[nombre],[segundonombre],
+		[apellido1],[apellido2],[correo],[clave],[celular],[rol])
+		VALUES (@cedulaProfesor,@nombre,@segundonombre,
+		@apellido1,@apellido2,@correo,@clave,@celular,@rol)
+		
+		INSERT INTO [dbo].[personal]([cedulaPersonal], [idSede], [telOficina], [foto])
+		VALUES(@cedulaProfesor, @idSede, @telOficina, @foto)
+
         INSERT INTO profesor (cedulaProfesor, codigo, esCordinador)
         VALUES (@cedulaProfesor, @codigo, @esCordinador)
 
-		INSERT INTO [dbo].[usuario]([cedulaUsuario],[nombre],[segundonombre],
-		[apellido1],[apellido2],[correo],[clave],[celular],[rol])
-		VALUES (@cedulaUsuario,@nombre,@segundonombre,
-		@apellido1,@apellido2,@correo,@clave,@celular,@rol)
-
-		INSERT INTO [dbo].[personal]([cedulaPersonal], [idSede], [telOficina], [foto])
-		VALUES(@cedulaPersonal, @idSede, @telOficina, @foto)
 
     END TRY
     BEGIN CATCH
+<<<<<<< refs/remotes/origin/main
         DECLARE @errMsg VARCHAR(200) = ERROR_MESSAGE();
 		THROW 50001, @errMsg, 1;
 >>>>>>> Procedure Crear Profesores
+=======
+        PRINT 'Ocurrio un error: ' + ERROR_MESSAGE()
+>>>>>>> CRUD y Crear profesor
     END CATCH
 END
 GO
