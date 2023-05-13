40adc849400bf3159a88721054dd3ab1fff3265f
Andres <anmasis@ic-itcr.ac.cr>
2023-05-13 17:44:22 -0600

    Backend Andres

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 require('dotenv').config(); //El dotenv es para poder utilizar el archivo .env (variables de entorno)
-const usuariosRoute = require("./Controladores/UsuarioController");
+const estudiantesRoute = require("./Controladores/EstudiantesController");
 
 //Settings
 app.set('port',process.env.PORT || 4000);
@@ -15,7 +15,7 @@ app.use(cors());
 
 //Rutas
 
-app.use("/usuarios",usuariosRoute);
+app.use("/estudiantes",estudiantesRoute);
 
 app.listen(app.get('port'),()=>{ //Va abrir el server en el puerto 4000
     console.log('Server on port',app.get('port'));
