ac314f5cef7f4de50d0d076f6a10a6a4d08ce070
Glori <75176699+globv28@users.noreply.github.com>
2023-06-15 08:52:20 -0600

    Chat lindo y funcional

--- Frontend/sistemaGuiaFrontend/src/components/Conversation.js
+++ Frontend/sistemaGuiaFrontend/src/components/Conversation.js
@@ -1,46 +1,99 @@
 import React, { useState } from 'react';
 import axios from "axios";
-const Conversation = ({messages, idChat}) => {
-    const [newMessage, setNewMessage] = useState({
-      idChat: idChat,
-      idParticipante: localStorage.getItem("cedula"),
-      mensaje: ''
-    });
-  
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        console.log("Mensaje", newMessage)
-        await axios.post('http://localhost:4000/chat/mensaje', newMessage);
-      } catch (err) {
-        console.error(err);
-      }
+const Conversation = ({ messages, idChat, nombre }) => {
+  const [newMessage, setNewMessage] = useState({
+    idChat: idChat,
+    idParticipante: localStorage.getItem("cedula"),
+    mensaje: ''
+  });
 
-    };
-  
-    return (
-      <div>
-        <h2>Conversación</h2>
-        <div>
-        {messages.map((mensaje) => (
-        <div key={mensaje.idMensaje} className="comment">
-          <p className="comment-author">{mensaje.idParticipante}</p>
-          <p className="comment-message">{mensaje.mensaje}</p>
-        </div>
-      ))}
-        </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={newMessage.mensaje}
-            onChange={(e) => setNewMessage({...newMessage, mensaje: e.target.value})}
-            placeholder="Escribe tu mensaje"
-          />
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log("Mensaje", newMessage)
+      await axios.post('http://localhost:4000/chat/mensaje', newMessage);
+    } catch (err) {
+      console.error(err);
+    }
   };
-  
-  export default Conversation;
+
+  return (
+    <div style={styles.container}>
+      <h2 style={styles.heading}>Conversación</h2>
+      <div style={styles.messageContainer}>
+        {messages.map((mensaje) => (
+          <div key={mensaje.idMensaje} style={styles.comment}>
+            <p style={styles.author}>{mensaje.idParticipante}</p>
+            <p style={styles.message}>{mensaje.mensaje}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} style={styles.form}>
+        <input
+          type="text"
+          value={newMessage.mensaje}
+          onChange={(e) => setNewMessage({ ...newMessage, mensaje: e.target.value })}
+          placeholder="Escribe tu mensaje"
+          style={styles.input}
+        />
+        <button type="submit" style={styles.button}>Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    backgroundColor: '#f5f5f5',
+    borderRadius: '4px',
+    padding: '16px',
+    maxWidth: '600px',
+    margin: '0 auto',
+  },
+  heading: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '16px',
+  },
+  messageContainer: {
+    maxHeight: '400px',
+    overflowY: 'auto',
+    marginBottom: '16px',
+  },
+  comment: {
+    backgroundColor: '#fff',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    padding: '8px',
+    marginBottom: '8px',
+  },
+  author: {
+    fontWeight: 'bold',
+    marginBottom: '4px',
+  },
+  message: {
+    marginBottom: 0,
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  input: {
+    flex: '1',
+    marginRight: '8px',
+    padding: '8px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    color: '#fff',
+    border: 'none',
+    padding: '8px 16px',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
+};
+
+export default Conversation;
--- Frontend/sistemaGuiaFrontend/src/components/ConversationList.js
+++ Frontend/sistemaGuiaFrontend/src/components/ConversationList.js
@@ -3,11 +3,14 @@ import React, { useState } from 'react';
 const ConversationList = ({ conversations, selectConversation }) => {
   return (
     <div>
-      <h2>Conversaciones</h2>
-      <ul>
+      <h2 style={styles.heading}>Conversaciones</h2>
+      <ul style={styles.list}>
         {conversations.map((conversation) => (
-          <li key={conversation.id}>
-            <button onClick={() => selectConversation(conversation.idChat)}>
+          <li key={conversation.id} style={styles.listItem}>
+            <button
+              onClick={() => selectConversation(conversation.idChat)}
+              style={styles.button}
+            >
               {conversation.nombre}
             </button>
           </li>
@@ -15,6 +18,29 @@ const ConversationList = ({ conversations, selectConversation }) => {
       </ul>
     </div>
   );
+};
+
+const styles = {
+  heading: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '16px',
+  },
+  list: {
+    listStyle: 'none',
+    padding: 0,
+  },
+  listItem: {
+    marginBottom: '8px',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    color: '#fff',
+    border: 'none',
+    padding: '8px 16px',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
 };
 
 export default ConversationList;
